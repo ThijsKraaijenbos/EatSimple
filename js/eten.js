c89e6c6d7b4a538b45dd531a60e7ff745a88f310
roshanmomand <1028086@hr.nl>
2024-03-27 13:23:12 +0100

    bezig met javascript. Eet buttons aangepast

--- js/eten.js
+++ js/eten.js
@@ -5,4 +5,9 @@ const init = () => {
     console.log("Alle elementen zijn geladen")
 }
 
+// maak een functie waar je de producten ophaalt van het anchor tag die je hebt gedrukt
+
+const showProducts = () => {
+    const productButton = document.getElementById("")
+}
 
