b734fb308d2ee099102fdccd74ab7068dc928f68
mvdwa <m.vdwaterlaat@gmail.com>
2024-03-28 10:40:00 +0100

    Added localstorage

--- js/task.js
+++ js/task.js
@@ -2,8 +2,8 @@ window.addEventListener('load', init);
 
 let id = new URLSearchParams(window.location.search).get('id'); // URLSearchParams haalt de url op
 let customUrl = "webservice/actions.php?id=" + id;
-let container // Roshan changed
-let main
+let container; // Roshan changed
+let main;
 
 function init() {
     getApi(customUrl, populateSite);
@@ -27,78 +27,92 @@ function populateSite(products) {
     // load all products of the anchor tag that has been clicked
     console.log(products);
     container = document.getElementById('container-producten');
-    main = document.querySelector("main")
+    main = document.querySelector("main");
 
-    createCart()
+    createCart();
 
 
     for (let product in products) {
         // create the elements
-        const productContainer = document.createElement("article")
-        const productName = document.createElement("h1")
+        const productContainer = document.createElement("article");
+        const productName = document.createElement("h1");
         const productButton = document.createElement('button');
-        const productImage = document.createElement("img")
+        const productImage = document.createElement("img");
 
 
         // Classes & ID
-        productImage.classList.add("product-image-size")
-        productButton.classList.add("product-button")
+        productImage.classList.add("product-image-size");
+        productButton.classList.add("product-button");
 
         // Dataset, Src, InnerText
-        productImage.src = products[product].img
-        productButton.dataset.name = products[product].name
+        productImage.src = products[product].img;
+        productButton.dataset.name = products[product].name;
+        productButton.dataset.type_id = products[product].type_id;
+        productButton.dataset.img = products[product].img;
         productButton.innerText = "Bestel";
-        productName.innerText = products[product].name
+        productName.innerText = products[product].name;
 
         // Eventlisteners
-        productButton.addEventListener("click", addToCart)
+        productButton.addEventListener("click", addToCart);
 
         // AppendSections
-        productContainer.appendChild(productImage)
-        productContainer.appendChild(productName)
+        productContainer.appendChild(productImage);
+        productContainer.appendChild(productName);
         productContainer.appendChild(productButton);
-        container.appendChild(productContainer)
-        main.appendChild(container)
+        container.appendChild(productContainer);
+        main.appendChild(container);
 
     }
 }
 
 
-function createCart(){
+function createCart() {
     // create Cart + cart Counter
-    const cartDiv = document.createElement("div")
-    const cartIcon = document.createElement("span")
-    let cartCounter = document.createElement("span")
+    const cartDiv = document.createElement("div");
+    const cartIcon = document.createElement("span");
+    let cartCounter = document.createElement("span");
 
     // Classes & Id
-    cartDiv.id = "add-to-cart"
-    cartIcon.id = "cart-icon"
-    cartCounter.id = "cart-count"
+    cartDiv.id = "add-to-cart";
+    cartIcon.id = "cart-icon";
+    cartCounter.id = "cart-count";
 
     // Dataset, Src, InnerText
-    cartCounter.innerText = " 0"
-    cartIcon.innerHTML = " &#128722;"
+    cartCounter.innerText = " 0";
+    cartIcon.innerHTML = " &#128722;";
 
 
     // AppendSections
-    cartDiv.appendChild(cartIcon)
-    cartDiv.appendChild(cartCounter)
-    main.appendChild(cartDiv)
+    cartDiv.appendChild(cartIcon);
+    cartDiv.appendChild(cartCounter);
+    main.appendChild(cartDiv);
 }
 
 function addToCart(e) {
-    console.log(e.target.dataset.name)
-    let productType = e.target.dataset.name
+    let productId = parseInt(e.target.dataset.type_id);
+    let productName = e.target.dataset.name;
+    let productAmount = 1;  // Default amount is 1
+    let productImg = e.target.dataset.img;
 
-    let cartCountElement = document.getElementById("cart-count")
-    cartCountElement.innerHTML = productType
+    let cartCountElement = document.getElementById("cart-count");
+    cartCountElement.innerHTML = productName;
 
-    // when you click the button add the item where you clicked to the cart
+    // Retrieve the cart from local storage or initialize it as an empty array
+    let cart = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : [];
 
+    // Check if the product already exists in the cart
+    let existingProduct = cart.find(item => item[0] === productId);
 
-    // Put the dataset in a variable and save it to localstorage in this case the cart
+    if (existingProduct) {
+        // If the product exists, increase the amount
+        existingProduct[2]++;
+    } else {
+        // If the product doesn't exist, add it to the cart
+        cart.push([productId, productName, productAmount, productImg]);
+    }
 
-
+    // Save the updated cart back to local storage
+    localStorage.setItem('cart', JSON.stringify(cart));
 }
 
 
