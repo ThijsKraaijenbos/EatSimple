c4fba2f623084e36ce9fd36310462de113c73b11
mvdwa <m.vdwaterlaat@gmail.com>
2024-03-28 14:26:25 +0100

    admin panel added

--- js/index.js
+++ js/index.js
@@ -1,8 +1,20 @@
 window.addEventListener("load", () => init())
 
+let home_page_button = document.querySelector('.home_page_button')
+home_page_button.addEventListener('click', openAdminPanel)
+
+let dialog = document.querySelector('.admin-modal')
+
 //Init functie
 const init = () => {
     console.log("Alle elementen zijn geladen")
 }
+
+function openAdminPanel() {
+    console.log('hi');
+    dialog.showModal();
+}
+
+
 
 
