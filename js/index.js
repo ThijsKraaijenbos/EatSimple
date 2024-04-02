15705b6feadd0b25bf99465efbf52b8c52417ce1
mvdwa <m.vdwaterlaat@gmail.com>
2024-04-02 09:55:27 +0200

    admin panel changed

--- js/index.js
+++ js/index.js
@@ -3,6 +3,13 @@ window.addEventListener("load", () => init())
 let home_page_button = document.querySelector('.home_page_button')
 home_page_button.addEventListener('click', openAdminPanel)
 
+let submit = document.querySelector('.submit')
+submit.addEventListener('click', submitButton)
+
+let username = document.querySelector('#username')
+let password = document.querySelector('#password')
+
+
 let dialog = document.querySelector('.admin-modal')
 
 //Init functie
@@ -11,8 +18,14 @@ const init = () => {
 }
 
 function openAdminPanel() {
-    console.log('hi');
     dialog.showModal();
+}
+
+function submitButton() {
+    if (username.value == 'admin' && password.value == 'admin') {
+        console.log('u mag inloggen');
+    }
+
 }
 
 
