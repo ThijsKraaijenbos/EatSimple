7d793e668fc990a53a32efadca39d8033e904278
Sem van Osch <semvanosch@gmail.com>
2024-04-08 12:40:45 +0200

    chef.html css

--- js/chef.js
+++ js/chef.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', init);
 
 let chefList = document.querySelector('.main');
+
 function init() {
     getApi('./webservice/data.php', populateSite);
 }
@@ -20,15 +21,20 @@ function getApi(url, nextFunction) {
 function populateSite(data) {
     console.log(data);
     for (let i = 0; i < data.length; i++) {
-        let productNameP = document.createElement("p")
-        productNameP.innerText =`Tafel: ${data[i].table} - Product naam: ${data[i].name} - Aantal: ${data[i].amount}`
-        chefList.appendChild(productNameP)
+        let div = document.createElement('div');
+        div.classList.add('chef-div');
 
-        let productButton = document.createElement("button")
-        productButton.innerText =`Klaar`
-        productButton.dataset.index = data[i].order_variant_id
-        productButton.addEventListener('click', orderReady)
-        chefList.appendChild(productButton)
+        let productNameP = document.createElement("p");
+        productNameP.innerText = `Tafel: ${data[i].table} - Product naam: ${data[i].name} - Aantal: ${data[i].amount}`;
+        div.appendChild(productNameP);
+
+        let productButton = document.createElement("button");
+        productButton.innerText = `Klaar`;
+        productButton.dataset.index = data[i].order_variant_id;
+        productButton.addEventListener('click', orderReady);
+        div.appendChild(productButton);
+
+        chefList.appendChild(div);
     }
 }
 
@@ -37,13 +43,13 @@ function errorMessage(data) {
 }
 
 function orderReady(e) {
-    console.log(e.target.dataset.index)
-    console.log('./webservice/editChef.php?id=' + e.target.dataset.index)
+    console.log(e.target.dataset.index);
+    console.log('./webservice/editChef.php?id=' + e.target.dataset.index);
     getApi('./webservice/editChef.php?id=' + e.target.dataset.index, next);//"webservice/actions.php?id=" + id;
     window.location.href = 'chef.html';
     window.close();
 }
 
 function next(data) {
-    console.log('item removed')
+    console.log('item removed');
 }
