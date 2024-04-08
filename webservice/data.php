475cecacf9894591bb71ef27cd347df035b23c61
mvdwa <m.vdwaterlaat@gmail.com>
2024-04-08 09:43:14 +0200

    chef page edited

--- webservice/data.php
+++ webservice/data.php
@@ -1,14 +1,26 @@
 <?php
-//require_once "webservice/actions.php";
-//
-////Based on the existence of the GET parameter, 1 of the 2 functions will be called
-//if (!isset($_GET['id'])) {
-//    $data = getFoodTypes();
-//} else {
-//    $data = getFoodDetails($_GET['id']);
-//}
-//
-////Set the header & output JSON so the client will know what to expect.
-//header("Content-Type: application/json");
+header('Content-Type: application/json'); // Set the content type to JSON
+require 'database.php';
+
+//Based on the existence of the GET parameter, 1 of the 2 functions will be called
+$stmt = '';
+
+    $stmt = $pdo->prepare("SELECT * FROM orders
+	INNER JOIN
+    order_variant ON orders.order_id = order_variant.order_id
+    INNER JOIN
+    types ON types.type_id = order_variant.product_id;");
+
+
+// Execute the prepared statement
+$stmt->execute();
+
+// Fetch all rows as an associative array
+$items = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+// Output the data as JSON
+echo json_encode($items);
+
+
 //echo json_encode($data);
-//exit();
+exit();
